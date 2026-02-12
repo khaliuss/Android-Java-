97b460c67cfe8ab4c50ebb842313a5ca57102151
khaliuss <khalidkhushmadov@gmail.com>
2026-02-12 16:29:07 +0300

    Messenger with Firebase

---
+++ MessengerFireBase/app/src/main/java/com/example/messengerfirebase/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.messengerfirebase;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        checkCurrentUser();
+
+
+    }
+
+    private void checkCurrentUser(){
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null){
+            Log.d(TAG,"Not authorized");
+        } else {
+            Log.d(TAG,"Authorized");
+        }
+    }
+
+    private void signUpUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+
+                        }else {
+
+                        }
+                    }
+                });
+    }
+
+
+    private void signInUser(String email, String password){
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+
+                        }else{
+
+                        }
+                    }
+                });
+    }
+
+
+}
