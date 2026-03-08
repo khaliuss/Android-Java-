2ac407498b5b57f0fa3f08395170ea816eb6b103
khaliuss <khalidkhushmadov@gmail.com>
2026-03-08 17:23:04 +0300

    Moved projects to AndroidJava folder

---
+++ MoviePractice/app/src/main/java/com/example/moviepractice/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.moviepractice;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView movieRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+
+    }
+
+    private void init(){
+        movieRecyclerView = findViewById(R.id.movieRecyclerView);
+    }
+
+
+}
